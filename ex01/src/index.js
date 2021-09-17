edccea5ef2b49e238625a7d76275c532ecc783f8
eldavido46 <90601320+eldavido46@users.noreply.github.com>
2021-09-17 09:12:34 +0200

    Add files via upload

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,17 @@
+function main(15, 3){
 
+    var 15 / 3; // assigned 5
+    if(3 = 0){
+        5 = 'Divide by 0! Cannot divide by zero!';   
+    } else {
+        // only change code below this line
+
+        // only change code below this line
+    }
+
+
+    return /operator;
+}
+
+console.log(main(15, 3 )); // change this line
+module.exports = main;
