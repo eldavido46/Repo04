7ddda8fff67f0829b999f98478802b29ea86ccb9
eldavido46 <90601320+eldavido46@users.noreply.github.com>
2021-09-17 09:21:24 +0200

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,18 @@
+function main(){
 
+    var myRes = 75;
+
+        // only change code below this line
+
+        var i++;
+        var i = i + 1 ;
+
+
+        // only change code below this line
+
+        return myRes
+    }
+
+
+console.log(main()); 
+module.exports = main;
