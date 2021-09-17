32caa81e48d2f0cf25271bd4b468f3aaf1026a26
eldavido46 <90601320+eldavido46@users.noreply.github.com>
2021-09-17 09:37:10 +0200

    Add files via upload

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,20 @@
+function main(2.5 , 2){
 
+        // only change code below this line
+
+        
+        *product = 5;
+        /operator = product 5 ;
+
+        
+
+        
+
+        // only change code below this line
+
+        return product;
+    }
+
+
+console.log(main()); 
+module.exports = main;
