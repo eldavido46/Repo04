a2efbd3325848bc85abfcbb6b1dde955f9865491
eldavido46 <90601320+eldavido46@users.noreply.github.com>
2021-09-17 09:30:37 +0200

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,22 @@
+function main(){
 
+        // only change code below this line
+
+        var myDecimal;
+        var myDecimal = 8.2;
+        var myDecimalE; 
+        var myDecimalE = 0.00254;
+
+        
+
+        // only change code below this line
+
+        return{
+            myDecimal;
+            myDecimalE
+        };
+    }
+
+
+console.log(main()); 
+module.exports = main;
