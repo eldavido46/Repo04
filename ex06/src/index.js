d1877236dd2bd4c152993dd73169497059f899ac
eldavido46 <90601320+eldavido46@users.noreply.github.com>
2021-09-17 09:40:09 +0200

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,19 @@
+function main(5.0 , 2.0){
 
+        // only change code below this line
+
+        var myQuotient
+    
+
+        
+
+        
+
+        // only change code below this line
+
+        return product;
+    }
+
+
+console.log(main()); 
+module.exports = main;
